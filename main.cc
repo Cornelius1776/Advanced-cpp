178210eaa44392120c8b53bc8a0e55d3e29b4bf6
Cornelius Mamman <corneliusisaac62@gmail.com>
2024-11-18 10:25:32 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,7 +1,25 @@
 #include <iostream>
 
+void test(const int a, const int b, std::unordered_map<int, int> &cache)
+{
+    const int key = a + b;
+    if (cache.find(key) != cache.end()) // since c++20 contains() can be used to perform the same task e.g cache.contains(key)
+    {
+        std::cout << "returning from cache: " << cache[key] << std::endl;
+        return;
+    }
+
+    const int result = key;
+    std::cout << "executing for the first time: " << result << std::endl;
+    cache[key] = result;
+}
+
 int main() {
-    std::cout << "Hello World" << std::endl;
+    test(2, 2, cache);
+    test(2, 2, cache);
+    test(3, 1, cache);
+    test(3, 7, cache);
+    test(5, 5, cache);
 
     return 0;
 }
